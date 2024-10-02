bf0f4186208ba0a032200c7a2603437e561bef0b
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-03 01:05:36 +0300

    todo silme

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,19 +2,29 @@ import { CiCircleRemove } from "react-icons/ci";
 import { CiCircleCheck } from "react-icons/ci";
 import { CiEdit } from "react-icons/ci";
 import { TodoType } from "../types/Types";
+import { removeTodoById } from "../redux/todoSlice";
+import { useDispatch } from "react-redux";
 
 interface TodoProps{
   todoProps:TodoType
 }
 
+
 function Todo({todoProps}:TodoProps) {
+  const dispatch=useDispatch();
   const{id,content}=todoProps;
+
+  const handleRemoveTodo=()=>{
+    dispatch(removeTodoById(id));
+
+  }
+
   return (
     <div style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between",border:"1px solid lightgrey",
     padding:"15px",marginTop:"25px",borderRadius:"5px"}}>
     <div>{content}</div>
       <div>
-      <CiCircleRemove className="icons" style={{marginRight:"5px"}}/>
+      <CiCircleRemove onClick={handleRemoveTodo}className="icons" style={{marginRight:"5px"}}/>
       <CiEdit className="icons"/>
       </div>
     </div>
