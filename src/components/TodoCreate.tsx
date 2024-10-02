a421600abc04b63df5b08dd58516b92727f17659
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-03 00:26:21 +0300

    todo oluşturma yapıldı

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,10 +1,34 @@
-
+import { useState } from "react";
+import { useDispatch } from "react-redux"
+import { createTodo } from "../redux/todoSlice";
+import { TodoType } from "../types/Types";
 
 function TodoCreate() {
+
+  const dispatch=useDispatch();
+  const [newTodo,setNewTodo]=useState<string>("");
+
+  const handleCreateTodo=()=>{
+    if(newTodo.trim().length==0){
+      alert("Todo giriniz!")
+      return;
+    }
+    const payload:TodoType={
+      id:Math.floor(Math.random()*999999999),
+      content:newTodo
+    }
+    dispatch(createTodo(payload));
+    setNewTodo("");
+
+}
+
   return (
     <div className="todo-create">
-        <input placeholder="Todo giriniz"className="todo-input"type="text"/>
-        <button className="todo-create-button">OLUŞTUR</button>
+        <input 
+        value={newTodo}
+        onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setNewTodo(e.target.value)}
+        placeholder="Todo giriniz"className="todo-input"type="text"/>
+        <button onClick={handleCreateTodo}className="todo-create-button">OLUŞTUR</button>
     </div>
   )
 }
