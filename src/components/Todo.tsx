471a0a4453c94bbd069eef33b71f84841d1f816c
Aybuke-Filiz <aybukeyavuz123@gmail.com>
2024-10-03 12:18:21 +0300

    finish

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,8 +2,9 @@ import { CiCircleRemove } from "react-icons/ci";
 import { CiCircleCheck } from "react-icons/ci";
 import { CiEdit } from "react-icons/ci";
 import { TodoType } from "../types/Types";
-import { removeTodoById } from "../redux/todoSlice";
+import { removeTodoById, updateTodo } from "../redux/todoSlice";
 import { useDispatch } from "react-redux";
+import { useContext, useState } from "react";
 
 interface TodoProps{
   todoProps:TodoType
@@ -14,18 +15,31 @@ function Todo({todoProps}:TodoProps) {
   const dispatch=useDispatch();
   const{id,content}=todoProps;
 
+  const[editable,setEditable]=useState<boolean>(false);
+  const[newTodo,setNewTodo]=useState<string>(content);
+
   const handleRemoveTodo=()=>{
     dispatch(removeTodoById(id));
 
   }
 
+  const handleUpdateTodo=()=>{
+    const payload:TodoType={
+      id,
+      content:newTodo
+    }
+    dispatch(updateTodo(payload))
+    setEditable(false);
+  }
+
   return (
     <div style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"space-between",border:"1px solid lightgrey",
     padding:"15px",marginTop:"25px",borderRadius:"5px"}}>
-    <div>{content}</div>
+      {editable?<input type="text" style={{width:"400px",border:"none",outline:"none",borderBottom:"1px solid lightgrey"}}
+      value={newTodo} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setNewTodo(e.target.value)}/>:<div>{content}</div>}
       <div>
       <CiCircleRemove onClick={handleRemoveTodo}className="icons" style={{marginRight:"5px"}}/>
-      <CiEdit className="icons"/>
+      {editable ? < CiCircleCheck onClick={handleUpdateTodo} className="icons"/>:<CiEdit onClick={()=>setEditable(!editable)}className="icons"/>}
       </div>
     </div>
   )
